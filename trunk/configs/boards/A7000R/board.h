98670bea1f35153e522e0a23f409335937cb3ca5
rusnazip <rusnazip@gmail.com>
2020-05-25 16:35:26 +0900

    add TOTOLINK A7000R

---
+++ trunk/configs/boards/A7000R/board.h
@@ -0,0 +1,34 @@
+/* TOTOLINK A7000R */
+
+#define BOARD_PID		"A7000R"
+#define BOARD_NAME		"A7000R"
+#define BOARD_DESC		"TOTOLINK A7000R Wireless Router"
+#define BOARD_VENDOR_NAME	"TOTOLINK"
+#define BOARD_VENDOR_URL	"https://www.totolink.net/"
+#define BOARD_MODEL_URL		"https://totolink.net/home/menu/newstpl/menu_newstpl/products/id/171.html"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME		120
+#undef BOARD_GPIO_BTN_RESET
+#undef BOARD_GPIO_BTN_WPS
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_SW2G
+#undef BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	18
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB
+#undef BOARD_GPIO_PWR_USB2
+#undef BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	4
+#define BOARD_NUM_ANT_5G_RX	4
+#define BOARD_NUM_ANT_2G_TX	4
+#define BOARD_NUM_ANT_2G_RX	4
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#undef BOARD_NUM_UPHY_USB3
+#undef BOARD_USB_PORT_SWAP
