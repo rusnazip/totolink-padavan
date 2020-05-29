d57933a1e4ea32207a97d967745eaffbf497fa95
rusnazip <rusnazip@gmail.com>
2020-05-29 14:07:09 +0900

    add A7100RU

---
+++ trunk/configs/boards/A7100RU/board.h
@@ -0,0 +1,35 @@
+/* A7100RU */
+
+#define BOARD_PID		"A7100RU"
+#define BOARD_NAME		"A7100RU"
+#define BOARD_DESC		"TOTOLINK A7100RU Wireless Router"
+#define BOARD_VENDOR_NAME	"TOTOLINK"
+#define BOARD_VENDOR_URL	"https://www.totolink.net/"
+#define BOARD_MODEL_URL		"https://totolink.ru/produkcziya/besprovodnyie-marshrutizatoryi/besprovodnyie-ac-marshrutizatoryi/a7100ru.html"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	4
+#define BOARD_GPIO_BTN_WPS	3
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define  BOARD_GPIO_LED_POWER	18
+#undef  BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB	7
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX 4
+#define BOARD_NUM_ANT_5G_RX 4
+#define BOARD_NUM_ANT_2G_TX 4
+#define BOARD_NUM_ANT_2G_RX 4
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#undef BOARD_USB_PORT_SWAP	0
